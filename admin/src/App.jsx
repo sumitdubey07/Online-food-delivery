bafb8af4fe322a17c749df22024fbdbe8aa093b5
sumitdubey07 <sd7055735@gmail.com>
2026-06-19 19:23:48 +0530

    Update admin API URL for production

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = 'http://localhost:4000';
+  const url = 'https://online-food-delivery-ijyt.onrender.com';
 
   return (
     <div>
